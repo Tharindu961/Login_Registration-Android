d1a56b8006aa63813f617804d06b1184c49d0008
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-05-14 11:03:07 +0530

    done the app

--- app/src/main/java/com/example/registraion_login_android/LoginActivity.java
+++ app/src/main/java/com/example/registraion_login_android/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -73,7 +74,13 @@ public class LoginActivity extends AppCompatActivity {
                     mFirebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
-                            
+                            if(!task.isSuccessful()){
+                                Toast.makeText(LoginActivity.this, "Login error, Please Login Again",Toast.LENGTH_SHORT).show();
+
+                            }else {
+                                Intent intToHome = new Intent(LoginActivity.this,HomeActivity.class);
+                                startActivity(intToHome);
+                            }
                         }
                     });
                 }
@@ -83,5 +90,20 @@ public class LoginActivity extends AppCompatActivity {
                     }
             }
         });
+
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intSignUp = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intSignUp);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
     }
 }
